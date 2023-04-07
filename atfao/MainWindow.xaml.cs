19ab05307008e82748dcb46e1f5f620a22b71037
moviefullframe <olegfedakov4046@gmail.com>
2023-04-07 10:37:49 +0300

    Добавьте файлы проекта.

---
+++ atfao/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace atfao
+{
+    public partial class MainWindow : Window
+    {
+        private atf_aoEntities db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new atf_aoEntities();
+
+            // Устанавливаем источник данных для DataGrid
+            DataGrid.ItemsSource = db.AccessEvents.ToList();
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Обработчик события нажатия кнопки "Экспорт".
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Обработчик события изменения выбранной строки в DataGrid.
+        }
+    }
+}
