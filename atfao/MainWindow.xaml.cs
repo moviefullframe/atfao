65758b4b453ac40b7061906a02142b758b633a28
moviefullframe <olegfedakov4046@gmail.com>
2023-04-07 12:20:43 +0300

    дата выбора данных

--- atfao/MainWindow.xaml.cs
+++ atfao/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Data.Entity;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,25 +8,75 @@ namespace atfao
 {
     public partial class MainWindow : Window
     {
-        private atf_aoEntities db;
+        private string connectionString = new atf_aoEntities().Database.Connection.ConnectionString;
+        private DataTable dataTable = new DataTable();
 
         public MainWindow()
         {
             InitializeComponent();
-            db = new atf_aoEntities();
 
-            // Устанавливаем источник данных для DataGrid
-            DataGrid.ItemsSource = db.AccessEvents.ToList();
+            // Загружаем данные по умолчанию
+            LoadData(DateTime.Today);
         }
 
-        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        private void LoadData(DateTime date)
         {
-            // Обработчик события нажатия кнопки "Экспорт".
+            // Создаем SQL-запрос для получения данных за выбранную дату
+            string sql = "SELECT [ID], [EventType], [ObjectName], [EventDateTime], [DirectionCode], [AccessPointPosition], [PassW26], [AccessPointID] FROM [AccessEvents] WHERE [EventDateTime] BETWEEN @startDate AND @endDate";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Создаем объект команды для выполнения запроса
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                // Добавляем параметры для запроса
+                command.Parameters.AddWithValue("@startDate", date.Date);
+                command.Parameters.AddWithValue("@endDate", date.AddDays(1).Date);
+
+                try
+                {
+                    // Открываем соединение и выполняем запрос
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    // Заполняем DataTable данными из результата запроса
+                    dataTable.Clear();
+                    dataTable.Load(reader);
+
+                    // Устанавливаем источник данных для DataGrid
+                    DataGrid.ItemsSource = dataTable.DefaultView;
+
+                    // Закрываем ридер и соединение
+                    reader.Close();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Обработчик события изменения выбранной строки в DataGrid.
+            var selectedDate = (sender as Calendar).SelectedDate;
+            if (selectedDate.HasValue)
+            {
+                var selectedDateString = selectedDate.Value.ToString("yyyy-MM-dd");
+                var query = $"SELECT * FROM [atf_ao].[dbo].[AccessEvents] WHERE CONVERT(DATE, [EventDateTime]) = '{selectedDateString}'";
+                var dataTable = new DataTable();
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    var adapter = new SqlDataAdapter(query, connection);
+
+
+                    adapter.Fill(dataTable);
+                }
+
+                DataGrid.ItemsSource = dataTable.DefaultView;
+            }
         }
+
     }
 }
